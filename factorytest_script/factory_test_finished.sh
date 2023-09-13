7b4e16b7cba7a3d3d87cbbae0c724e20e9d73312
Jax <fuliby2loveyou@gmail.com>
2023-09-13 10:26:55 +0800

    add args agingtest enabled  for factory_test_finished

--- factorytest_script/factory_test_finished.sh
+++ factorytest_script/factory_test_finished.sh
@@ -4,12 +4,26 @@
 # 当前版本：V1.0
 
 # 描述:
-#   测试完成并将老化脚本添加到开机启动
+#   测试完成，
+#   关闭ADB或者将老化脚本添加到开机启动
 
 # 参数说明:
-#   无需提供任何参数。
+#   -A | --agingtest   启用老化测试
 
 # 文件和目录路径
+
+# 定义Usage函数
+usage() {
+    echo "使用方法："
+    echo "  $0 [-A | --agingtest]"
+    echo
+    echo "参数说明:"
+    echo "  -A | --agingtest   启用老化测试"
+    exit 1
+}
+
+AGINGTEST_ENABLED=false
+
 AGING_STAMP=/aging_start_stamp
 AGING_SCRIPT_SRC=/tmp/factorytest_bin/AgingTest
 AGING_SCRIPT_DEST=/usr/share/AgingTest
@@ -19,35 +33,61 @@ UDEV_RULES_SRC=/tmp/factorytest_script/99-usb-config.rules
 UDEV_RULES_DEST=/etc/udev/rules.d/99-usb-config.rules
 RC_LOCAL_FILE=/etc/rc.local
 
-# 创建 aging_start_stamp 文件
-touch "$AGING_STAMP" && echo "Created $AGING_STAMP"
+# 解析命令行参数
+while [[ $# -gt 0 ]]; do
+    case "$1" in
+    -A | --agingtest)
+        AGINGTEST_ENABLED=true
+        shift
+        ;;
+    *)
+        usage
+        ;;
+    esac
+done
 
-# 同步文件系统
-sync && echo "Filesystem synced"
+if [[ "$AGINGTEST_ENABLED" == true ]]; then
+    # 创建 aging_start_stamp 文件
+    touch "$AGING_STAMP" && echo "Created $AGING_STAMP"
 
-# 复制老化脚本
-cp "$AGING_SCRIPT_SRC" "$AGING_SCRIPT_DEST" && echo "Copied $AGING_SCRIPT_SRC to $AGING_SCRIPT_DEST"
+    # 同步文件系统
+    sync && echo "Filesystem synced"
 
-# 复制 USB 配置脚本和 udev 规则
-cp "$USB_CONFIG_SRC" "$USB_CONFIG_DEST" && cp "$UDEV_RULES_SRC" "$UDEV_RULES_DEST" && echo "Copied USB configuration files to destination"
+    # 复制老化脚本
+    cp "$AGING_SCRIPT_SRC" "$AGING_SCRIPT_DEST" && echo "Copied $AGING_SCRIPT_SRC to $AGING_SCRIPT_DEST"
 
-# 授予执行权限
-chmod 777 "$AGING_SCRIPT_DEST" && chmod 777 "$USB_CONFIG_DEST" && echo "Changed permissions for $AGING_SCRIPT_DEST and $USB_CONFIG_DEST"
+    # 复制 USB 配置脚本和 udev 规则
+    cp "$USB_CONFIG_SRC" "$USB_CONFIG_DEST" && cp "$UDEV_RULES_SRC" "$UDEV_RULES_DEST" && echo "Copied USB configuration files to destination"
 
-# 同步文件系统
-sync && echo "Filesystem synced"
+    # 授予执行权限
+    chmod 777 "$AGING_SCRIPT_DEST" && chmod 777 "$USB_CONFIG_DEST" && echo "Changed permissions for $AGING_SCRIPT_DEST and $USB_CONFIG_DEST"
 
-# 要添加到 /etc/rc.local 的命令
-START_AGING_TEST="[ -f $AGING_STAMP ] && $AGING_SCRIPT_DEST"
+    # 同步文件系统
+    sync && echo "Filesystem synced"
 
-# 检查是否已添加到 /etc/rc.local
-if ! grep -qF "$START_AGING_TEST" "$RC_LOCAL_FILE"; then
-    # 使用 sed 在 /etc/rc.local 的末尾添加命令
-    sed -i "\$i$START_AGING_TEST" "$RC_LOCAL_FILE" && echo "Command added to $RC_LOCAL_FILE"
+    # 要添加到 /etc/rc.local 的命令
+    START_AGING_TEST="[ -f $AGING_STAMP ] && $AGING_SCRIPT_DEST"
+
+    # 检查是否已添加到 /etc/rc.local
+    if ! grep -qF "$START_AGING_TEST" "$RC_LOCAL_FILE"; then
+        # 使用 sed 在 /etc/rc.local 的末尾添加命令
+        sed -i "\$i$START_AGING_TEST" "$RC_LOCAL_FILE" && echo "Command added to $RC_LOCAL_FILE"
+    fi
+
+    # 同步文件系统
+    sync && echo "Filesystem synced"
+else
+    echo host >/sys/devices/platform/fe8a0000.usb2-phy/otg_mode
+
+    # 要添加到 /etc/rc.local 的命令
+    OTG_MODE_HOST="echo host >/sys/devices/platform/fe8a0000.usb2-phy/otg_mode"
+
+    # 检查是否已添加到 /etc/rc.local
+    if ! grep -qF "$OTG_MODE_HOST" "$RC_LOCAL_FILE"; then
+        # 使用 sed 在 /etc/rc.local 的末尾添加命令
+        sed -i "\$i$OTG_MODE_HOST" "$RC_LOCAL_FILE" && echo "Command added to $RC_LOCAL_FILE"
+    fi
 fi
-
-# 同步文件系统
-sync && echo "Filesystem synced"
 
 # 打印完成消息
 echo "Factory Test Aging Script Configuration Completed."
